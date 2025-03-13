61993bf03e21c65c74ffb71c96185bde5e95d5f2
damphone <amphonedara03@gmail.com>
2025-03-13 00:11:13 -0500

    implement RR algorithm

--- algorithms/RR.js
+++ algorithms/RR.js
@@ -1,4 +1,41 @@
 export function rrScheduler(processes, timeQuantum){
-    
+    const sortedProcesses = [...processes].sort((a, b) => a.arrivalTime - b.arrivalTime);
 
+    let currentTime = 0;
+    let schedule = [];
+    let queue = [...sortedProcesses];
+
+    while (queue.length > 0) {
+        const process = queue.shift();
+
+        if (process.arrivalTime > currentTime) {
+            currentTime = process.arrivalTime;
+        }
+
+        let startTime = currentTime;
+        let remainingBurstTime = process.burstTime - timeQuantum;
+
+        if (remainingBurstTime > 0) {
+            currentTime += timeQuantum;
+            queue.push({
+                ...process,
+                burstTime: remainingBurstTime,
+                startTime,
+                completionTime: null,
+                turnaroundTime: null,
+                waitingTime: null,
+            });
+        } else {
+            currentTime += process.burstTime;
+            schedule.push({
+                ...process,
+                startTime,
+                completionTime: currentTime,
+                turnaroundTime: currentTime - process.arrivalTime,
+                waitingTime: currentTime - process.arrivalTime - process.burstTime,
+            });
+        }
+    }
+
+    return schedule;
 }
