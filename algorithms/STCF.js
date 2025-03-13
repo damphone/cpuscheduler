a69b5bd6fdaaf3d136c68e40dd85c88565b7247a
damphone <amphonedara03@gmail.com>
2025-03-12 21:15:44 -0500

    implement STCF algorithm

--- algorithms/STCF.js
+++ algorithms/STCF.js
@@ -1,3 +1,35 @@
 export function stcfScheduler(processes){
-    
+    const sortedProcesses = [...processes].sort((a, b) => a.arrivalTime - b.arrivalTime);
+
+    let currentTime = 0;
+    let schedule = [];
+
+    while (sortedProcesses.length > 0) {
+        const readyProcesses = sortedProcesses.filter(process => process.arrivalTime <= currentTime);
+        if (readyProcesses.length === 0) {
+            currentTime++;
+            continue;
+        }
+
+        const shortestProcess = readyProcesses.reduce((prev, curr) => prev.burstTime < curr.burstTime ? prev : curr);
+        const index = sortedProcesses.indexOf(shortestProcess);
+        sortedProcesses.splice(index, 1);
+
+        let startTime = Math.max(currentTime, shortestProcess.arrivalTime);
+        let completionTime = startTime + shortestProcess.burstTime;
+        let turnaroundTime = completionTime - shortestProcess.arrivalTime;
+        let waitingTime = turnaroundTime - shortestProcess.burstTime;
+
+        schedule.push({
+            ...shortestProcess,
+            startTime,
+            completionTime,
+            turnaroundTime,
+            waitingTime,
+        });
+
+        currentTime = completionTime;
+    }
+
+    return schedule;
 }
